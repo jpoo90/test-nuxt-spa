d64fed09b0e677de2426a8b90874290207aaa444
jpoo90 <jpoo90@gmail.com>
2018-09-30 14:49:05 -0400

    Example

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="links">
+		<nuxt-link to="one">One</nuxt-link>
+		<nuxt-link to="some/two">Two</nuxt-link>
+		<nuxt-link to="some/three">Three</nuxt-link>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.links {
+	display: flex;
+	justify-content: space-around;
+}
+</style>
